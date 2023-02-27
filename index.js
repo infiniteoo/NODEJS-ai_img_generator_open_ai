78f887da1a9ab8b9e7d3a2158b368189505569ab
Troy Dorman <troydorman@gmail.com>
2023-02-27 00:17:21 -0700

    created generateimage post route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use("/openai", require("./routes/openaiRoutes"));
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
